854db2204da186fda5cefabe78f1aaa2a12ad44a
RubenFern <rubenferval@hotmail.com>
2021-04-27 21:35:41 +0200

    Cambio de ubicación

---
+++ src/components/app/public/HomeUserPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomeUserPage = () => 
+{
+    return (
+        <div>
+            <h1 className="text-light">Home del usuario </h1>
+        </div>
+    )
+}
---
+++ src/components/app/public/ImagesUserPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ImagesUserPage = () => 
+{
+    return (
+        <div>
+            <h1 className="text-light">Imágenes del usuario</h1>
+        </div>
+    )
+}
