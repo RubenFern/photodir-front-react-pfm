02903c8e91917edc91e2ba3a75f10dc6f670db33
RubenFern <rubenferval@hotmail.com>
2021-04-28 19:35:07 +0200

    Fichero para almacenar las importaciones de los componentes de las rutas

---
+++ src/routers/componentsRoutes.js
@@ -0,0 +1,16 @@
+import { LoginScreen } from '../components/login/LoginScreen';
+import { RegisterScreen } from '../components/login/RegisterScreen';
+import { HomeUserPage } from '../components/app/public/HomeUserPage';
+import { ImagesUserPage } from '../components/app/public/ImagesUserPage';
+import { HomePage } from '../components/app/auth/HomePage';
+import { ImagesPage } from '../components/app/auth/ImagesPage';
+
+export const routes =
+{
+    LoginScreen,
+    RegisterScreen,
+    HomePage,
+    ImagesPage,
+    HomeUserPage,
+    ImagesUserPage
+}
