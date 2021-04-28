c4ea6739827669434e948b6d63a9722d13e525d9
RubenFern <rubenferval@hotmail.com>
2021-04-28 22:32:11 +0200

    Ningun cambio

--- src/components/auth/authReducer.js
+++ src/components/auth/authReducer.js
@@ -2,6 +2,10 @@ import { types } from "../types/types";
 
 export const authReducer = (state = {}, action) => 
 {
+    /**
+     * Retorno siempre un booleano para saber si el usuario está o no conectado. En el login añado un 
+     * payload para poder almacenar cualquier datos adicional.
+    */ 
     switch (action.type) {
         case types.login:
             
