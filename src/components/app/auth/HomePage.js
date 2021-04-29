7c4212bbfe8ef12b2f0b873063383ac4ae0bde58
RubenFern <rubenferval@hotmail.com>
2021-04-29 21:09:03 +0200

    Ningun cambio

--- src/components/app/auth/HomePage.js
+++ src/components/app/auth/HomePage.js
@@ -5,6 +5,11 @@ export const HomePage = () => {
     return (
         <>
             <NavBar />
+
+            <div className="container mt-5">
+                <h1 className="text-light">Álbumes del usuario</h1>
+
+            </div>
         </>
     )
 }
