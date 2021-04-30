aff8b0c395d68cc089db51483b1513ecabc2e9b4
RubenFern <rubenferval@hotmail.com>
2021-05-01 00:06:16 +0200

    Registro de usuarios hecho. Validación de campos implementada

--- src/components/login/RegisterScreen.js
+++ src/components/login/RegisterScreen.js
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { validatePassword } from '../../helpers/validatePassword';
 import { useForm } from '../../hooks/useForm';
-import { AuthContext } from '../auth/AuthContext';
+import { AuthContext } from '../../hooks/useContext';
 import { NavBar } from '../layout/NavBar';
+import { register as authRegister } from '../auth/auth';
 
 export const RegisterScreen = ({ history }) => 
 {
+    // Creo el state para validar los campos del formulario
+    const [valid, setvalid] = useState(true);
+
     const [state, handleInputChange] = useForm({
         name: '',
         user_name: '',
@@ -18,26 +23,34 @@ export const RegisterScreen = ({ history }) =>
 
     const { name, user_name, email, password, password2, image } = state;
 
-
     const { user: {message}, dispatch } = useContext(AuthContext);
 
 
-    const register = (e) =>
+    const register = async(e) =>
     {
         e.preventDefault();
-
-        console.log(state)
 
         const validFields = validatePassword(password, password2);
 
         // Si las contraseñas coinciden dejo seguir el proceso de registro
         if (validFields)
         {
-            console.log("contraseñas iguales")
+            setvalid(true);
         } else
         {
-            console.log("Contraseñas noooooooooo")
+            setvalid(false);
+            return;
         }
+
+        // Si algún campo está vacío marco el error
+        if (name === '' || user_name === '' || email === '' || password === '')
+        {
+            setvalid(false);
+            return;
+        }
+
+        // Realizo la petición a la API para añadir el usuario
+        await authRegister(state, dispatch, history);
     }
 
     return (
@@ -60,11 +73,14 @@ export const RegisterScreen = ({ history }) =>
                             type="text" 
                             id="name" 
                             name="name"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && name === '') && "is-invalid" } `} 
                             placeholder="Introduce tu nombre"
                             onChange={handleInputChange}
                             value={name}
                         />
+                        <div className="invalid-feedback">
+                            Debes introducir un nombre
+                        </div>
                     </div>
                     <br />
                     <div className="form-group">
@@ -73,11 +89,14 @@ export const RegisterScreen = ({ history }) =>
                             type="text" 
                             id="user_name" 
                             name="user_name"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && user_name === '') && "is-invalid" } `} 
                             placeholder="Introduce un nombre de usuario"
                             onChange={handleInputChange}
                             value={user_name}
                         />
+                        <div className="invalid-feedback">
+                            El nombre de usuario no es válido
+                        </div>
                     </div>
                     <br />
                     <div className="form-group">
@@ -86,11 +105,14 @@ export const RegisterScreen = ({ history }) =>
                             type="text" 
                             id="email" 
                             name="email"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && email === '') && "is-invalid" } `}  
                             placeholder="Introduce tu correo electrónico"
                             onChange={handleInputChange}
                             value={email}
                         />
+                        <div className="invalid-feedback">
+                            El correo electrónico no es valido
+                        </div>
                     </div>
                     <br />
                     <div className="form-group has-validation">
@@ -99,14 +121,14 @@ export const RegisterScreen = ({ history }) =>
                             type="password" 
                             id="password"
                             name="password"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && password === '') && "is-invalid" } `} 
                             placeholder="Introduce una contraseña"
                             autoComplete="on"
                             onChange={handleInputChange}
                             value={password} 
                         />
                         <div className="invalid-feedback">
-                            Please choose a username.
+                            Debes introducir una contraseña
                         </div>
                     </div>
                     <br />
@@ -116,19 +138,19 @@ export const RegisterScreen = ({ history }) =>
                             type="password" 
                             id="password2"
                             name="password2"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && (password !== password2)) && "is-invalid" } `}  
                             placeholder="Introduce una contraseña"
                             autoComplete="on"
                             onChange={handleInputChange}
                             value={password2} 
                         />
                         <div className="invalid-feedback">
-                            Please choose a username.
+                            La contraseña no coincide
                         </div>
                     </div>
                     <br />
                     <div className="form-group">
-                        <label htmlFor="image">Foto de Perfil:</label>
+                        <label htmlFor="image">Foto de Perfil: (Opcional)</label>
                         <input 
                             type="file" 
                             id="image" 
@@ -145,6 +167,11 @@ export const RegisterScreen = ({ history }) =>
                             Iniciar Sesión
                         </button>
                     </div>
+                    <p
+                        className="text-center m-auto mt-4"
+                    >
+                        ¿Ya tienes una cuenta? <Link to="/login" className="link"> Inicia Sesión</Link>
+                    </p>
                 </form>
             </div>
         </>
