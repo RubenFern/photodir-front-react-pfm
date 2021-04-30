dec1b3b87740e7cc3e89d14ea2117e2f3b1a301d
RubenFern <rubenferval@hotmail.com>
2021-05-01 00:04:24 +0200

    Registro de usuarios funcionando

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -49,9 +49,47 @@ const login = async(user_name, password, dispatch, history) =>
     }
 }
 
-const register = () =>
+const register = async(data, dispatch, history) =>
 {
+    const res = await fetchNoToken('registro', data, 'POST');
 
+    const { logged, message, token, user, errors = [] } = await res.json();
+
+    if (logged)
+    {
+        const { image, is_admin: isAdmin, name, user_name: userName, uid, creation_date: registerDate } = user;
+
+        localStorage.setItem('token', token);
+
+        dispatch({
+            type: types.login,
+            payload: {
+                image, 
+                is_admin: isAdmin, 
+                name, 
+                user_name: userName, 
+                uid, 
+                creation_date: registerDate
+            }
+        });
+
+        history.replace('/home');
+    } else if (errors.length > 0)
+    {
+        dispatch({
+            payload: {
+                message: errors[0].msg
+            }
+        });
+
+    } else
+    {
+        dispatch({
+            payload: {
+                message
+            }
+        });
+    }
 }
 
 export 
