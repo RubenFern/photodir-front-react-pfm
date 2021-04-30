877b79a34f5a5d1d0ce475f4bfe108d90bbe50fc
RubenFern <rubenferval@hotmail.com>
2021-05-01 00:05:41 +0200

    Cambio en la llamada del context

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { useForm } from '../../hooks/useForm';
-import { AuthContext } from '../auth/AuthContext';
+import { AuthContext } from '../../hooks/useContext';
 import { NavBar } from '../layout/NavBar';
 import { login as authLogin } from '../auth/auth';
 
 import './LoginScreen.css';
+import { Link } from 'react-router-dom';
 
 
 export const LoginScreen = ({ history }) => 
 {
+    // Creo el state para validar los campos del formulario
+    const [valid, setvalid] = useState(true);
+
     const [state, handleInputChange] = useForm({
         user_name: '',
         password: ''
@@ -24,6 +28,13 @@ export const LoginScreen = ({ history }) =>
     const login = async(e) =>
     {
         e.preventDefault();
+
+        // Si algún campo está vacío marco el error
+        if (user_name === '' || password === '')
+        {
+            setvalid(false);
+            return;
+        }
 
         // Llamo al método de autenticación para guardar los datos de sesión del usuario en toda la aplicación
         await authLogin(user_name, password, dispatch, history);       
@@ -49,11 +60,14 @@ export const LoginScreen = ({ history }) =>
                             type="text" 
                             id="user_name" 
                             name="user_name"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && user_name === '') && "is-invalid" } `}
                             placeholder="Introduce tu nombre de usuario"
                             onChange={handleInputChange}
                             value={user_name}
                         />
+                        <div className="invalid-feedback">
+                            Debes introducir tu nombre de usuario
+                        </div>
                     </div>
                     <br />
                     <div className="form-group">
@@ -62,12 +76,15 @@ export const LoginScreen = ({ history }) =>
                             type="password" 
                             id="password"
                             name="password"
-                            className="form-control" 
+                            className={`form-control ${ (!valid && password === '') && "is-invalid" } `} 
                             placeholder="Introduce tu contraseña"
                             autoComplete="on"
                             onChange={handleInputChange}
                             value={password} 
                         />
+                        <div className="invalid-feedback">
+                            Debes introducir tu contraseña
+                        </div>
                     </div>
                     <br />
                     <div className="form-group submit">
@@ -75,6 +92,11 @@ export const LoginScreen = ({ history }) =>
                             Iniciar Sesión
                         </button>
                     </div>
+                    <p
+                        className="text-center m-auto mt-4"
+                    >
+                        ¿No tienes una cuenta? <Link to="/registro" className="link"> Regístrate</Link>
+                    </p>
                 </form>
             </div>
         </>
