709544528267e7a9a7fd9059ad4f52a209325d65
RubenFern <rubenferval@hotmail.com>
2021-05-16 20:54:32 +0200

    Página de administrador creada

---
+++ src/components/app/admin/AdminPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { NavBar } from '../../layout/NavBar';
+
+export const AdminPage = () => 
+{
+    return (
+        <>
+            <NavBar />
+
+
+            <h1 className="text-light">ADMIN</h1>
+        </>
+    )
+}
